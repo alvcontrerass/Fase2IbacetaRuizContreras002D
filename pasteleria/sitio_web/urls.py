b219360d249d4d367d34059f20b3d21c41858643
Sonia Ibaceta <sonia_patricia3161@hotmail.com>
2020-10-30 19:00:26 -0300

    Pasteleria: Creando Formularios

--- pasteleria/sitio_web/urls.py
+++ pasteleria/sitio_web/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',views.inicio,name='index'),
     path('catalogo/',views.catalogo,name='catalogo'),
     path('contacto/',views.contacto,name='contacto'),
+    path('enviar_formulario/', views.enviar_formulario, name='enviar_formulario')
 ]
--- pasteleria/sitio_web/views.py
+++ pasteleria/sitio_web/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def inicio(request):
@@ -19,3 +21,27 @@ def contacto(request):
         request,
         'formulario.html'
     )
+
+def enviar_formulario(request):
+
+    if request.method=="POST":
+        
+        nombre   = request.POST["nombre"]
+        email    = request.POST["email"]
+        telefono = request.POST["telefono"]
+        fecha    = request.POST["fecha"]
+        comment  = request.POST["comentario"]
+
+        emai_from=settings.EMAIL_HOST_USER
+        recipient_list=['momentosdulces.pasteleriaa@gmail.com']
+
+        subject = "Contacto desde formulario WEB"
+        message = 'Se ha recibido un contacto desde la página WEB \nNombre: %s Email: %s \nTelefono: %s \nFecha: %s \nComentario: \n %s' % (nombre, email, telefono, fecha, comment)
+
+        send_mail(subject, message, emai_from, recipient_list)
+        
+        return render (
+        request,
+        'formulario.html',
+        context={}
+        )
