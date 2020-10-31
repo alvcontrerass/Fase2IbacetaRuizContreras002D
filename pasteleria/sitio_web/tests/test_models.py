2b7e295daab64900df99052106312b1f904aa4f4
Alv.contrerass <alvarocontrerasilva@gmail.com>
2020-10-31 02:28:27 -0300

    Prueba realizada

---
+++ pasteleria/sitio_web/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from . models import Producto, Pedido_posicion
+
+class PastelesTestCase(TestCase):
+    def setup(self):
+        a1=Producto.objects.create(codigo_producto=5)
+        a2=Producto.objects.create(codigo_producto=2)
+        Pedido_posicion.objects.create(codigo_produto=a1, cantidad=1)
+        Pedido_posicion.objects.create(codigo_produto=a2, cantidad=2)
+
+    def test_pastel_pedido(self):
+        pedido1 = Pedido_posicion.objects.get(cantidad=1)
+        self.assertEqual(pedido1.Producto.codigo_producto, 5)
