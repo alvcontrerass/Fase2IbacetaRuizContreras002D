c30141abe69f106e367330dc6c1ccc2263fe86b9
Maribel Ruiz <70784188+maruizr@users.noreply.github.com>
2020-10-31 20:14:17 -0300

    Delete admin.py

--- sitio_web/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . models import Producto , Pedido_cabecera , Pedido_posicion
-
-admin.site.register(Producto)
-admin.site.register(Pedido_cabecera)
-admin.site.register(Pedido_posicion)
